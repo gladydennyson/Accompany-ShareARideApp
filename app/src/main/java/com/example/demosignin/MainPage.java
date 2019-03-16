73e422b207e2871a964e1dc3941d07f514d151eb
gladydennyson <gladydennyson184@gmail.com>
2019-03-16 18:21:23 +0530

    User login changed with Google signin authentication for chatroom.

--- app/src/main/java/com/example/demosignin/MainPage.java
+++ app/src/main/java/com/example/demosignin/MainPage.java
@@ -1,6 +1,7 @@
 package com.example.demosignin;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -17,12 +18,24 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 
 public class MainPage extends AppCompatActivity implements LocationListener {
 
+    FirebaseAuth Auth;
     final Context context = this;
     private Button button;
     private Button getLocation;
@@ -90,7 +103,57 @@ public class MainPage extends AppCompatActivity implements LocationListener {
                 gochatroom.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        startActivity(new Intent(MainPage.this, Login.class));
+                        Auth= FirebaseAuth.getInstance();
+                        final FirebaseUser userf = Auth.getCurrentUser();
+                        final String displayname = userf.getDisplayName();
+                        Log.w("display name", displayname);
+                        if(userf!=null)
+                        {
+                            String url = "https://costoptimized.firebaseio.com/users.json";
+                            final ProgressDialog pd = new ProgressDialog(MainPage.this);
+                            pd.setMessage("Loading...");
+                            pd.show();
+
+                            StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String s) {
+                                    if (s.equals("null")) {
+                                        Toast.makeText(MainPage.this, "user not found", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        try {
+                                            JSONObject obj = new JSONObject(s);
+
+                                            if (!obj.has(displayname)) {
+                                                Toast.makeText(MainPage.this, "inside user not found", Toast.LENGTH_LONG).show();
+                                            }  else {
+                                                UserDetails.username = displayname;
+                                                ///UserDetails.password = pass;
+                                                startActivity(new Intent(MainPage.this, Users.class));
+                                                //Toast.makeText(Login.this, "incorrect password", Toast.LENGTH_LONG).show();
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+
+                                    pd.dismiss();
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError volleyError) {
+                                    System.out.println("" + volleyError);
+                                    pd.dismiss();
+                                }
+                            });
+
+                            RequestQueue rQueue = Volley.newRequestQueue(MainPage.this);
+                            rQueue.add(request);
+                            //startActivity(new Intent(MainPage.this, Login.class));
+                        }
+                        else{
+                            Log.w("User not logged in","bad");
+                        }
+                        //startActivity(new Intent(MainPage.this, Login.class));
                     }
                 });
                 dialog.show();
