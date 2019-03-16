a57ce2f625654cb2bf29dd6a9f4f1e5459c6fdcb
gladydennyson <gladydennyson184@gmail.com>
2019-03-16 20:20:08 +0530

    Register substituted with Google Sign-in

--- app/src/main/java/com/example/demosignin/MainActivity.java
+++ app/src/main/java/com/example/demosignin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.demosignin;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.firebase.client.Firebase;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -23,13 +31,16 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
 
     FirebaseAuth auth;
     GoogleSignInClient mGoogleSignInClient;
     SignInButton signInButton;
-    String displayname;
+    String displayname,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(signInIntent, 1);
             }
         });
+
+        Firebase.setAndroidContext(this);
 
     }
 
@@ -89,11 +102,63 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
 
+
+
                             FirebaseUser user = auth.getCurrentUser();
                             displayname = user.getDisplayName();
+
                             Toast.makeText(MainActivity.this, displayname.toString(), Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(MainActivity.this, MainPage.class);
-                            startActivity(intent);
+
+                            pass="thispwd";
+                            final ProgressDialog pd = new ProgressDialog(MainActivity.this);
+                            pd.setMessage("Loading...");
+                            pd.show();
+
+                            String url = "https://costoptimized.firebaseio.com/users.json";
+
+                            StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+                                @Override
+                                public void onResponse(String s) {
+                                    Firebase reference = new Firebase("https://costoptimized.firebaseio.com/users");
+
+                                    if(s.equals("null")) {
+                                        reference.child(displayname).child("password").setValue(pass);
+                                        Toast.makeText(MainActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(MainActivity.this, MainPage.class);
+                                        startActivity(intent);
+                                    }
+                                    else {
+                                        try {
+                                            JSONObject obj = new JSONObject(s);
+
+                                            if (!obj.has(displayname)) {
+                                                reference.child(displayname).child("password").setValue(pass);
+                                                Toast.makeText(MainActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                                                Intent intent = new Intent(MainActivity.this, MainPage.class);
+                                                startActivity(intent);
+                                            } else {
+                                                Toast.makeText(MainActivity.this, "username already exists", Toast.LENGTH_LONG).show();
+                                            }
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+
+                                    pd.dismiss();
+                                }
+
+                            },new Response.ErrorListener(){
+                                @Override
+                                public void onErrorResponse(VolleyError volleyError) {
+                                    System.out.println("" + volleyError );
+                                    pd.dismiss();
+                                }
+                            });
+
+                            RequestQueue rQueue = Volley.newRequestQueue(MainActivity.this);
+                            rQueue.add(request);
+
                         }
 
                         else {
